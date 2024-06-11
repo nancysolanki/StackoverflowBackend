e2a4132de6b4e59235d4cea69a3a3504b7ac1135
NancySolanki <nnancy.ssolanki@gmail.com>
2024-06-11 22:39:01 +0530

    Adapter is Deprecated

---
+++ src/main/java/com/stackoverflow/backend/Security/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.stackoverflow.backend.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
