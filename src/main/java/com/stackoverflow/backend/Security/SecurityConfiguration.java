9701a6aa12a6e2071e17dbb1796f42c5d85c3028
NancySolanki <nnancy.ssolanki@gmail.com>
2024-06-12 23:46:08 +0530

    trying httpSecurity

--- src/main/java/com/stackoverflow/backend/Security/SecurityConfiguration.java
+++ src/main/java/com/stackoverflow/backend/Security/SecurityConfiguration.java
@@ -3,7 +3,9 @@ package com.stackoverflow.backend.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 
 @Configuration
@@ -17,4 +19,15 @@ public class SecurityConfiguration {
             .build();
         return new InMemoryUserDetailsManager(user);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests(authorize -> authorize
+            .antMatchers("api/ques/home").permitAll()
+            .antMatchers("/api/ques/**").hasAnyRole("USER", "ADMIN")
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .anyRequest().authenticated()
+        )
+    } 
 }
