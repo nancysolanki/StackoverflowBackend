d9d6bc3682dcb68f92280cbc3bdb6aceb6602dea
NancySolanki <nnancy.ssolanki@gmail.com>
2024-06-12 23:54:24 +0530

    trying httpSecurity2

--- src/main/java/com/stackoverflow/backend/Security/SecurityConfiguration.java
+++ src/main/java/com/stackoverflow/backend/Security/SecurityConfiguration.java
@@ -2,14 +2,15 @@ package com.stackoverflow.backend.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
@@ -23,11 +24,15 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests(authorize -> authorize
-            .antMatchers("api/ques/home").permitAll()
-            .antMatchers("/api/ques/**").hasAnyRole("USER", "ADMIN")
-            .antMatchers("/admin/**").hasRole("ADMIN")
-            .anyRequest().authenticated()
-        )
-    } 
+        .authorizeHttpRequests((authz) -> authz
+        .requestMatchers("/api/home").permitAll()
+        .requestMatchers("/api/ques/**").hasAnyRole("USER", "ADMIN")
+        .requestMatchers("/admin/**").hasRole("ADMIN")
+        .anyRequest().authenticated()
+    ).formLogin(form -> form
+    .permitAll()
+)
+    .httpBasic();
+  return http.build();
+    }
 }
